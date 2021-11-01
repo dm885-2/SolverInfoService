8319d462276085e3254810602f8d51601a7ee1fc
Michael <clonex_kontakt@hotmail.com>
2021-11-01 08:21:51 +0100

    Updated example-test to check JSON content

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,8 @@
 import {ping} from "./index.js";
 
-test('ping responds with pong', async () => {
+test('ping responds with pong', () => {
     ping({token: "",}, (channel, message) => {
         expect(channel).toBe("pong");
+        expect(message.check).toBe(false);
     });
 });
