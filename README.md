237bbd627081863126e5c782b31c7882235b7665
Michael <clonex_kontakt@hotmail.com>
2021-11-05 09:38:00 +0100

    Added mysql library

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Microservice-template
 A template for a generic microservice, which already have most stuff setup needed for a generic service.
-- RapidMQ, Verify JWT token.
+- DB connection.
+- RapidMQ.
+- Verify JWT token.
 - JEST testing.
 - NPM scripts.
 
@@ -20,3 +22,6 @@ npm run test
 |----------|-----------------------------------------|
 | riverUrl | Contains the URL to the RapidMQ server. |
 | SECRET   | Contains the JWT secret.                |
+| mysqlHost| Contains the mysql host.                |
+| mysqlUser| Contains the mysql user.                |
+| mysqlPass| Contains the mysql pass.                |
--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,6 @@
 import jwt from "jsonwebtoken";
+import mysql from "mysql";
+
 
 const SECRET = process.env.SECRET ?? `3(?<,t2mZxj$5JT47naQFTXwqNWP#W>'*Kr!X!(_M3N.u8v}%N/JYGHC.Zwq.!v-`;  // JWT secret
 export const host = process.env.riverUrl ?? `amqp://localhost`;  // RabbitMQ url
@@ -11,4 +13,27 @@ export const host = process.env.riverUrl ?? `amqp://localhost`;  // RabbitMQ url
 export function getTokenData(token)
 {
     return new Promise(resolve => jwt.verify(token, SECRET, (err, data) => resolve(err ? false : data)));
+}
+
+let connection;
+if(process.env.mysqlHost)
+{
+    connection = mysql.createConnection({
+        host     : process.env.mysqlHost,
+        user     : process.env.mysqlUser,
+        password : process.env.mysqlPass,
+        database : 'db'
+    });
+    connection.connect();
+}
+
+/**
+ * Runs a SQL query on the DB. 
+ * @param string stmt 
+ * @param ?string[] WHERE 
+ * @returns 
+ */
+export function query(stmt, WHERE = [])
+{
+    return new Promise(r => connection.query(stmt, WHERE, (err, results) => r(results, err)));
 }
