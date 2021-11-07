2d354405a27d7fce61ea7a431af599b756df08d1
smessie <smessie@smessie.com>
2021-11-07 19:42:02 +0100

    Move Express files under src/

--- src/app.js
+++ src/app.js
@@ -4,8 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const indexRouter = require('./routes');
 
 const app = express();
 
@@ -19,8 +18,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Define all different routers.
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
