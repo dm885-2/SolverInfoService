e300b4581714c302d065564194840f682987200e
EmilOvcina <44057269+EmilOvcina@users.noreply.github.com>
2021-12-10 19:29:00 +0100

    Update test_services.sh

--- test_services.sh
+++ test_services.sh
@@ -1,5 +1,25 @@
-# TEST_GATEWAY
-kubectl -n rabbits apply -f https://raw.githubusercontent.com/EmilOvcina/RabbitMQcluster/main/app/deployment.yaml
+# ------- UN-COMMENT THE SERVICES YOU WANT TO TEST WITH THIS SERVICE ------- #
 
-# TEST SQL Thing
-kubectl -n rabbits apply -f https://raw.githubusercontent.com/EmilOvcina/RabbitMQcluster/main/test-deployment.yaml
+                  #   DONT UNCOMMENT YOUR OWN SERVICE HERE    #
+# -------------------------------------------------------------------------- #          
+          
+# GATEWAY
+kubectl -n rabbits apply -f https://raw.githubusercontent.com/DM885/GatewayService/main/deployment.yaml
+
+# CRUD SERVICE
+# kubectl -n rabbits apply -f https://raw.githubusercontent.com/DM885/CRUDservice/main/deployment.yaml
+
+# SERVICE INFO SERVICE
+# kubectl -n rabbits apply -f https://raw.githubusercontent.com/DM885/SolverInfoService/main/deployment.yaml
+
+# AUTH SERVICE
+# kubectl -n rabbits apply -f https://raw.githubusercontent.com/DM885/AuthenticationService/main/deployment.yaml
+
+# LOGGING SERVICE
+# kubectl -n rabbits apply -f https://raw.githubusercontent.com/DM885/LoggingService/main/deployment.yaml
+
+# JOB SERVICE
+# kubectl -n rabbits apply -f https://raw.githubusercontent.com/DM885/JobService/main/deployment.yaml
+
+# MINIZINC SERVICE
+# kubectl -n rabbits apply -f https://raw.githubusercontent.com/DM885/MiniZincService/main/deployment.yaml
