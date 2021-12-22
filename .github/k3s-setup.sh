e497b6ff03c8a98339bbb473e6d590e734195644
= <=>
2021-12-22 10:36:03 +0100

    updated to handle private repos

--- .github/k3s-setup.sh
+++ .github/k3s-setup.sh
@@ -2,21 +2,23 @@
 helm repo add bitnami https://charts.bitnami.com/bitnami
 
 # Storage Class
-kubectl apply -f https://raw.githubusercontent.com/DM885/RabbitMQK8S/main/rabbitmq-statefulset-sc.yaml
+curl -s -u EmilOvcina:$1 https://api.github.com/repos/DM885/RabbitMQK8S/contents/rabbitmq-statefulset-sc.yaml |jq -r ".content" |base64 --decode | kubectl apply -f -
 
 # # NAMESPACES
 kubectl create ns rabbits
 
 # MYSQL
-kubectl apply -f https://raw.githubusercontent.com/DM885/MySQLK8S/main/mysql-secrets.yaml
-kubectl -n rabbits apply -f https://raw.githubusercontent.com/DM885/MySQLK8S/main/mysql-secrets.yaml
-kubectl -n rabbits apply -f https://raw.githubusercontent.com/DM885/MySQLK8S/main/auth-secrets.yaml
-kubectl apply -f https://raw.githubusercontent.com/DM885/MySQLK8S/main/mysql-pv.yaml
-kubectl apply -f https://raw.githubusercontent.com/DM885/MySQLK8S/main/mysql-pvc.yaml
-helm install mysql -f https://raw.githubusercontent.com/DM885/MySQLK8S/main/mysql-values.yaml bitnami/mysql
+curl -s -u EmilOvcina:$1 https://api.github.com/repos/DM885/MySQLK8S/contents/mysql-secrets.yaml |jq -r ".content" |base64 --decode | kubectl apply -f -
+curl -s -u EmilOvcina:$1 https://api.github.com/repos/DM885/MySQLK8S/contents/mysql-secrets.yaml |jq -r ".content" |base64 --decode | kubectl -n rabbits apply -f -
+curl -s -u EmilOvcina:$1 https://api.github.com/repos/DM885/MySQLK8S/contents/auth-secrets.yaml |jq -r ".content" |base64 --decode | kubectl -n rabbits apply -f -
+curl -s -u EmilOvcina:$1 https://api.github.com/repos/DM885/MySQLK8S/contents/mysql-pv.yaml |jq -r ".content" |base64 --decode | kubectl apply -f -
+curl -s -u EmilOvcina:$1 https://api.github.com/repos/DM885/MySQLK8S/contents/mysql-pvc.yaml |jq -r ".content" |base64 --decode | kubectl apply -f -
+
+curl -s -u EmilOvcina:$1 https://api.github.com/repos/DM885/MySQLK8S/contents/mysql-values.yaml |jq -r ".content" |base64 --decode > tmp1
+helm install mysql -f tmp1 bitnami/mysql
 
 # # RABBITMQ 
-kubectl -n rabbits apply -f https://raw.githubusercontent.com/DM885/RabbitMQK8S/main/rabbit-rbac.yaml
-kubectl -n rabbits apply -f https://raw.githubusercontent.com/DM885/RabbitMQK8S/main/rabbit-configmap.yaml
-kubectl -n rabbits apply -f https://raw.githubusercontent.com/DM885/RabbitMQK8S/main/rabbit-secret.yaml
-kubectl -n rabbits apply -f https://raw.githubusercontent.com/DM885/RabbitMQK8S/main/rabbit-statefulset.yaml
+curl -s -u EmilOvcina:$1 https://api.github.com/repos/DM885/RabbitMQK8S/contents/rabbit-rbac.yaml |jq -r ".content" |base64 --decode | kubectl -n rabbits apply -f -
+curl -s -u EmilOvcina:$1 https://api.github.com/repos/DM885/RabbitMQK8S/contents/rabbit-configmap.yaml |jq -r ".content" |base64 --decode | kubectl -n rabbits apply -f -
+curl -s -u EmilOvcina:$1 https://api.github.com/repos/DM885/RabbitMQK8S/contents/rabbit-secret.yaml |jq -r ".content" |base64 --decode | kubectl -n rabbits apply -f -
+curl -s -u EmilOvcina:$1 https://api.github.com/repos/DM885/RabbitMQK8S/contents/rabbit-statefulset.yaml |jq -r ".content" |base64 --decode | kubectl -n rabbits apply -f -
